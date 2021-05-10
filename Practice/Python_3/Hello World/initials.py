66574caf20d4e24a38d1a9f9299fc9562b3846e4
AlexPlex1 <61334247+AlexPlex1@users.noreply.github.com>
2021-05-10 11:18:04 +0300

    Create initials.py

---
+++ Practice/Python_3/Hello World/initials.py
@@ -0,0 +1,12 @@
+# Alexandros Plexidas
+# i love my dog
+first_letter="""
+     A         P P P P
+   A   A       P       P
+ A       A     P       P
+ A A A A A     P P P P
+ A       A     P
+ A       A     P
+ A       A     P
+ """
+print(first_letter)
